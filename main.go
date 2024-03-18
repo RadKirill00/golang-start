7e6bbddfd5c1f92b5db0670ec051a13cb86c743e
RadKirill00 <radkirill00@mail.ru>
2024-03-18 15:18:55 +0300

    qwe

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import "fmt"
-
-type Point struct {
-	X int
-	Y int
-	T string
-}
-
-func (p Point) method() {
-	fmt.Println(p.X)
-	fmt.Println(p.Y)
-}
-
 func main() {
-	str := Point{
-		X: 2,
-		Y: 3,
-		T: "Hello, GO",
-	}
-	str.method()
-	fmt.Println(str)
+
 }
