62b6c87cc7bd8a7eae7793b97b1a673528bd983b
RadKirill00 <radkirill00@mail.ru>
2024-03-20 14:52:46 +0300

    qwe

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func something(callback func(int, int) int, s string) int {
+	res := callback(5, 5)
+	fmt.Println(s)
+	return res
+}
+
+func main() {
+	callbackFunc := func(n int, q int) int { return n + q }
+
+	res := something(callbackFunc, "Hello, Kirill Radchenko")
+
+	fmt.Println(res)
 }
