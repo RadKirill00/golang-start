c34458193e1315448e08637b02fbdc0245a7d719
RadKirill00 <radkirill00@mail.ru>
2024-05-05 13:50:25 +0300

    My first server

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+)
+
+type User struct {
+	Name    string
+	Age     int
+	Gender  string
+	Hobbies []string
+}
 
 func main() {
-	var a, b int
-	fmt.Scan(&a, &b)
-
-	sum := a + b
-	fmt.Println(sum)
+	user := User{"Kirill", 17, "Man", []string{"Football", "Reading the books", "Play on PS4"}}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, _ := template.ParseFiles("golang-start/index.html")
+		tmpl.Execute(w, user)
+	})
+	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+		tmpl2, _ := template.ParseFiles("golang-start/contact.html")
+		tmpl2.Execute(w, nil)
+	})
+	http.ListenAndServe(":8080", nil)
 }
